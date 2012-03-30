bc7722763260c60e311764ac671604be77a208b4
quocfreelancer <quocfreelancer@gmail.com>
2012-03-30 15:54:00 +0000

    update so much think on prototype

--- Code/SkJukeBox-Demo/MainWindow.xaml.cs
+++ Code/SkJukeBox-Demo/MainWindow.xaml.cs
@@ -24,6 +24,9 @@ namespace SkJukeBox_Demo
         private int count = 10;
         private ObservableCollection<string> tempStrings;
         ObservableCollection<string> strings = new ObservableCollection<string>() { "aaa", "vvv", "ccc", "aaa1", "vvv1", "ccc1", "aaa2", "vvv2", "ccc2", "aaa3", "vvv3", "ccc3", "aaa", "vvv", "ccc", "aaa1", "vvv1", "ccc1", "aaa2", "vvv2", "ccc2", "aaa3", "vvv3", "ccc3", "aaa", "vvv", "ccc", "aaa1", "vvv1", "ccc1", "aaa2", "vvv2", "ccc2", "aaa3", "vvv3", "ccc3", "aaa", "vvv", "ccc", "aaa1", "vvv1", "ccc1", "aaa2", "vvv2", "ccc2", "aaa3", "vvv3", "ccc3", "aaa", "vvv", "ccc", "aaa1", "vvv1", "ccc1", "aaa2", "vvv2", "ccc2", "aaa3", "vvv3", "ccc3", "aaa", "vvv", "ccc", "aaa1", "vvv1", "ccc1", "aaa2", "vvv2", "ccc2", "aaa3", "vvv3", "ccc3", "aaa", "vvv", "ccc", "aaa1", "vvv1", "ccc1", "aaa2", "vvv2", "ccc2", "aaa3", "vvv3", "ccc3", "aaa", "vvv", "ccc", "aaa1", "vvv1", "ccc1", "aaa2", "vvv2", "ccc2", "aaa3", "vvv3", "ccc3", "aaa", "vvv", "ccc", "aaa1", "vvv1", "ccc1", "aaa2", "vvv2", "ccc2", "aaa3", "vvv3", "ccc3", "aaa", "vvv", "ccc", "aaa1", "vvv1", "ccc1", "aaa2", "vvv2", "ccc2", "aaa3", "vvv3", "ccc3", "aaa", "vvv", "ccc", "aaa1", "vvv1", "ccc1", "aaa2", "vvv2", "ccc2", "aaa3", "vvv3", "ccc3", "aaa", "vvv", "ccc", "aaa1", "vvv1", "ccc1", "aaa2", "vvv2", "ccc2", "aaa3", "vvv3", "ccc3", "aaa", "vvv", "ccc", "aaa1", "vvv1", "ccc1", "aaa2", "vvv2", "ccc2", "aaa3", "vvv3", "ccc3", };
+
+        private int keyCount = 0;
+        private string inputKey;
         public MainWindow()
         {
             InitializeComponent();
@@ -31,7 +34,40 @@ namespace SkJukeBox_Demo
             this.MusicListBox.ItemsSource = tempStrings;
         }
 
-        private void Pre_Click(object sender, RoutedEventArgs e)
+        private void CloseContextMenu_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void CloseCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+            e.Handled = true;
+        }
+
+        private void CloseExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            this.Close();
+            e.Handled = true;
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.D1 || e.Key == Key.D2 || e.Key == Key.D3 || e.Key == Key.D4 || e.Key == Key.D5 || e.Key == Key.D6 || e.Key == Key.D7 || e.Key == Key.D8 || e.Key == Key.D9 || e.Key == Key.D0)
+            {
+                this.keyCount++;
+                inputKey += e.Key;
+                inputKey = inputKey.Replace("D", string.Empty);
+                if (this.keyCount == 5)
+                {
+                    MessageBox.Show(this.inputKey);
+                    this.keyCount = 0;
+                    this.inputKey = string.Empty;
+                }
+            }
+        }
+
+        private void Pre_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (index != 0)
             {
@@ -41,7 +77,7 @@ namespace SkJukeBox_Demo
             }
         }
 
-        private void Next_Click(object sender, RoutedEventArgs e)
+        private void Next_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (index < this.strings.Count)
             {
